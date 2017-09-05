46177f4eceb2ccf132296bb61c800fb2d69d17df
vamps <pmsz@ukr.net>
2017-09-05 18:27:29 +0300

    calc3

--- calc_metlife/main.js
+++ calc_metlife/main.js
@@ -1,13 +1,16 @@
-	let vklad = parseInt(document.getElementById('vklad').value);
-	var termin = parseInt(document.getElementById('termin').value);
-	var procent = parseInt(document.getElementById('procent').value);
-	var procent = procent / 100;
-
+	
 	
 	
 	function fn() {
+		let vklad = parseInt(document.getElementById('vklad').value);
+		var termin = parseInt(document.getElementById('termin').value);
+		var procent = parseInt(document.getElementById('procent').value);
+		var procent = procent / 100;
+
 		var rez = 0;
 		var x = 0;
+		console.log(vklad);
+	
 		while(x<termin) 	{
 				rez = rez + vklad;
 				rez = parseInt(rez +(rez*procent));
