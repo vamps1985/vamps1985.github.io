320e28db92c07fb6a5f641bae250322c12c1f953
vamps <pmsz@ukr.net>
2017-09-29 12:41:06 +0300

    JS5

--- JS7/main.js
+++ JS7/main.js
@@ -1,15 +1,33 @@
-$("#addfield").click(function(event) {
-  addfield();
-  return false;
-
-})
-
-function addfield() {
-  let div = $("<div/>",{
-  class: 'DynamicExtraField')}.appendTo(div);
-  var br = $('<br/>').appendTo(div);
-  $('#addField').click(function(event) {
-    addfield();
+$('#addDynamicExtraFieldButton').click(function(event) {
+    addDynamicExtraField();
     return false;
  });
- 
+function addDynamicExtraField() {
+    var div = $('<div/>', {
+        'class' : 'DynamicExtraField'
+    }).appendTo($('#DynamicExtraFieldsContainer'));
+    var br = $('<br/>').appendTo(div);
+
+    var textarea = $('<input/>', {
+        name : 'DynamicExtraField[]',
+        'class' : 'inp'
+    }).appendTo(div);
+    var input = $('<input/>', {
+        value : '-',
+        type : 'button',
+        'class' : 'DeleteDynamicExtraField'
+    }).appendTo(div);
+    input.click(function() {
+        $(this).parent().remove();
+    });
+}
+
+$("#getInf").click(function(event) {
+  var list = $(".inp").value;
+  console.log(list);
+
+    return false;
+ });
+ var listv = [];
+list = $("#getInf").val;
+console.log(list);
