5fb0074c7d9e8d4b1f01d768c72fac91a5b80555
vamps <pmsz@ukr.net>
2017-10-02 11:27:22 +0300

    js7

--- JS7/main.js
+++ JS7/main.js
@@ -1,50 +1,68 @@
 $('#addDynamicExtraFieldButton').click(function(event) {
-    addDynamicExtraField();
-    return false;
- });
+  addDynamicExtraField();
+  return false;
+});
+
 function addDynamicExtraField() {
-    var div = $('<div/>', {
-        'class' : 'DynamicExtraField'
-    }).appendTo($('#DynamicExtraFieldsContainer'));
-    var br = $('<br/>').appendTo(div);
-    var textarea = $('<input/>', {
-        name : 'DynamicExtraField[]',
-        'class' : 'inp'
-    }).appendTo(div);
-    var input = $('<input/>', {
-        value : '-',
-        type : 'button',
-        'class' : 'DeleteDynamicExtraField'
-    }).appendTo(div);
+  var div = $('<div/>', {
+    'class': 'DynamicExtraField'
+  }).appendTo($('#DynamicExtraFieldsContainer'));
+  var br = $('<br/>').appendTo(div);
+  var textarea = $('<input/>', {
+    name: 'DynamicExtraField[]',
+    'class': 'inp'
+  }).appendTo(div);
+  var input = $('<input/>', {
+    value: '-',
+    type: 'button',
+    'class': 'DeleteDynamicExtraField'
+  }).appendTo(div);
 
-    var zero = $('<span/>', {
-        html : '',
-        type : 'text',
-        'class' : 'notinfo'
-    }).appendTo(div);
+  var zero = $('<span/>', {
+    html: '',
+    type: 'text',
+    'class': 'notinfo'
+  }).appendTo(div);
 
-    input.click(function() {
-        $(this).parent().remove();
-    });
-}
+  input.click(function() {
+    $(this).parent().remove();
+  });
+};
 $("#getInf").click(function(event) {
 
-  $.each($('.inp'),function(){
-    if (this.value.length<1) {
-     this.style.border = "2px solid red";
-     $("span").html("Заполните");
-
-   } else {
-     this.style.border = "";
-      $("span").del;
-
-   }
-    });
-  let values = [];
-  $.each($('.inp'),function(){
-    if (this.value.length>0) {
-      values+=this.value + " " ;
+  $.each($('.inp'), function() {
+    if (this.value.length==0) {
+      this.style.border = "2px solid red";
+      let z = $(this).siblings("span");
+      z.html(" Заполните");
+    } else {
+      this.style.border = "";
+      $(this).siblings("span").html("");
     }
-    });
+  });
+  let values = [];
+  $.each($('.inp'), function() {
+    if (this.value.length > 0) {
+      values += this.value + " ";
+    }
+  });
   $("#rez").val(values);
+});
+
+
+$("#check1:radio").on("change", function(){
+  $(".inp1").attr('class', 'inp');
+  $(".inp:even").attr('class', 'inp1');
+
+
  });
+
+$("#check2:radio").on("change", function(){
+    $(".inp1").attr('class', 'inp');
+  $(".inp:odd").attr('class', 'inp1');
+});
+
+
+
+$("#check3:radio").on("change", function(){
+    $(".inp1").attr('class', 'inp'); });
