ad48b70c2f0688d50d1e07bd5bd4e1718294a54f
vamps <pmsz@ukr.net>
2017-10-03 20:33:41 +0300

    js8

--- JS8/main.js
+++ JS8/main.js
@@ -1,4 +1,5 @@
 $("#pas").click(function () {
+  let spisok = ["1","2","3","4","5","6","7","8","9","0","q","w","e","r","t","y","i","o","p","a","s","d","f","g","h","j","k","l","z","x","c","v","b","n","m"];
   let inp = $("#inp").val();
   inp = inp.replace( /http:\/\//g, "" );
   inp = inp.replace( /https:\/\//g, "" );
@@ -9,10 +10,14 @@ $("#pas").click(function () {
   inp = inp.replace( /o/g, "0" );
   inp = inp.replace( /i/g, "3" );
   inp = inp.replace( /s/g, "5" );
-  let ran1 = Math.random().toString(36);
+  let x1 = Math.floor(Math.random() * spisok.length);
+  let x2 = Math.floor(Math.random() * spisok.length);
+  let x3 = Math.floor(Math.random() * spisok.length);
+  let x4 = Math.floor(Math.random() * spisok.length);
 
-  console.log(ran1);
-  console.log(inp);
+
+  let pas = (inp+spisok[x1]+spisok[x2]+spisok[x3]+spisok[x4]);
+  $("#rez").html(pas);
 
 
 });
