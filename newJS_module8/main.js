f007a19e5d02372db49c15b2d279cfe07395e0d6
Vova Grytsun <pmsz@ukr.net>
2017-11-07 23:10:54 +0200

    js8

---
+++ newJS_module8/main.js
@@ -0,0 +1,67 @@
+/*
+ Напишите скрипт который реализует следующее поведение:
+
+ - При нажатии на клавишу (не виртуальной клавиатуры) должно
+  обрабатываться событие keydown.
+  (Для обработки нажатия нажатия клавиш, повесьте слушателя на window.
+  window.addEventListener("keydown", callback);)
+
+ - Должны обрабатываться только те клавиши, которые присутствуют
+  в разметке HTML (на виртуальной клавиатуре).
+
+ - Звук нажатия на клавишу должен соответсвовать ноте, описанной
+  в атрибуте button data-note.
+
+ - Подсветку текущей клавиши реализуйте с помощью класса
+  keyboard__btn--active.
+
+ - Чекбокс Sound должен включать и выключать звук нажатия на клавиши.
+*/
+
+const playSound = note => {
+  const audio = document.querySelector(`audio[data-note=${note}]`);
+  audio.currentTime = 0;
+  audio.play();
+};
+
+const buttons = Array.from(document.querySelectorAll("button"));
+const keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./".split("");
+
+window.onkeydown = function (event) {
+
+
+let z = event.key;
+
+
+let arr = document.querySelectorAll("button.keyboard__btn");
+for (let x = 0; x < arr.length; x++) {
+  if ((arr[x].innerHTML)==z) {
+    let muz = document.getElementsByTagName('audio');
+
+    arr[x].classList.add("keyboard__btn--active");
+    let but_sound = arr[x].getAttribute("data-note");
+
+    for (var v = 0; v < muz.length; v++) {
+      let s = muz[v].getAttribute("data-note");
+      if (but_sound==s) {
+
+      muz[v].play();
+      
+        //s.setAttribute(play,);
+
+      }
+
+
+
+    }
+
+  }
+
+}
+};
+window.onkeyup = function (event) {
+  let arr1 = document.querySelectorAll("button.keyboard__btn.keyboard__btn--active");
+    for (var c = 0; c < arr1.length; c++) {
+      arr1[c].classList.remove("keyboard__btn--active")
+    }
+};
