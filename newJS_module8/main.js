fa509bd18a9f5f5f8bcaeb05ccf0e3e11083d087
Vova Grytsun <pmsz@ukr.net>
2017-11-08 14:52:26 +0200

    js8

--- newJS_module8/main.js
+++ newJS_module8/main.js
@@ -23,48 +23,36 @@ const playSound = note => {
   audio.currentTime = 0;
   audio.play();
 };
-
 const buttons = Array.from(document.querySelectorAll("button"));
 const keys = "qwertyuiop[]asdfghjkl;'zxcvbnm,./".split("");
+let mass = keys.push(" ");
 
-window.onkeydown = function (event) {
-
-
-let z = event.key;
-
-
-let arr = document.querySelectorAll("button.keyboard__btn");
-for (let x = 0; x < arr.length; x++) {
-  if ((arr[x].innerHTML)==z) {
-    let muz = document.getElementsByTagName('audio');
-
-    arr[x].classList.add("keyboard__btn--active");
-    let but_sound = arr[x].getAttribute("data-note");
-
-    for (var v = 0; v < muz.length; v++) {
-      let s = muz[v].getAttribute("data-note");
-      if (but_sound==s) {
-        muz[v].pause();
-        muz[v].currentTime = 0;
-
-
-      muz[v].play();
-
-
-
+window.onkeydown = function(event) {
+  let z = event.key;
+  let q = event.code;
+  let arr = document.getElementsByTagName('button');
+  for (let x = 0; x < arr.length; x++) {
+    if ((keys[x]) == z) {
+      let muz = document.getElementsByTagName('audio');
+      arr[x].classList.add("keyboard__btn--active");
+      let but_sound = arr[x].getAttribute("data-note");
+      let slideThree = document.getElementById('slideThree');
+      if (slideThree.checked) {
+        for (var v = 0; v < muz.length; v++) {
+          let s = muz[v].getAttribute("data-note");
+          if (but_sound == s) {
+            muz[v].pause();
+            muz[v].currentTime = 0;
+            muz[v].play();
+          }
+        }
       }
-
-
-
     }
-
   }
-
-}
 };
-window.onkeyup = function (event) {
+window.onkeyup = function(event) {
   let arr1 = document.querySelectorAll("button.keyboard__btn.keyboard__btn--active");
-    for (var c = 0; c < arr1.length; c++) {
-      arr1[c].classList.remove("keyboard__btn--active")
-    }
+  for (var c = 0; c < arr1.length; c++) {
+    arr1[c].classList.remove("keyboard__btn--active");
+  }
 };
